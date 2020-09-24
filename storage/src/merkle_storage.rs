1567264f05f86f1ce84228c37b9e3ab03b36eda9
Michal Stanek <michal.stanek@simplestaking.com>
2020-09-25 01:48:53 +0200

    Use HashMap instead of OrdMap in merkle storage
    Remove external dependency and use standard HashMap. It seems OrdMap was
    contributing to high memory use.
    
    Once we stabilize memory usage we can look at replacing it with immutable
    HashMap from im crate if it has better performance.

--- storage/src/merkle_storage.rs
+++ storage/src/merkle_storage.rs
@@ -46,7 +46,6 @@ use std::path::Path;
 use rocksdb::{DB, ColumnFamilyDescriptor, Options, IteratorMode};
 use crate::persistent::{default_table_options, KeyValueSchema};
 use std::hash::Hash;
-use im::OrdMap as OrdMap;
 use serde::Deserialize;
 use serde::Serialize;
 use std::collections::HashMap;
@@ -71,7 +70,7 @@ struct Node {
     entry_hash: EntryHash,
 }
 
-type Tree = OrdMap<String, Node>;
+type Tree = HashMap<String, Node>;
 
 #[derive(Debug, Hash, Clone, Serialize, Deserialize)]
 struct Commit {
