f40955238ca52a0fc5cb2bc42b620a9057b42217
Alexander Koptelov <alexandre.koptelov@gmail.com>
2020-11-07 13:56:32 +0300

    Introduce nom-based type aliases

--- tezos/encoding/src/de_nom.rs
+++ tezos/encoding/src/de_nom.rs
@@ -4,16 +4,18 @@ use nom::{
     Finish,
 };
 
+pub type NomInput<'a> = &'a [u8];
+pub type NomResult<'a, T> = IResult<NomInput<'a>,T>;
 
-pub trait DeserializeNom: Sized {
-    fn nom_parse<'a>(i: &'a [u8]) -> IResult<&'a [u8],Self>;
+pub trait NomDeserialize: Sized {
+    fn nom_parse<'a>(i: NomInput<'a>) -> NomResult<'a, Self>;
 }
 
 pub trait NomFrom<'a>: Sized {
     fn nom_from_bytes(i: &'a [u8]) -> Result<Self,BinaryReaderError>;
 }
 
-impl<'a, T> NomFrom<'a> for T where T: DeserializeNom {
+impl<'a, T> NomFrom<'a> for T where T: NomDeserialize {
     fn nom_from_bytes(i: &'a [u8]) -> Result<Self,BinaryReaderError> {
         match Self::nom_parse(i).finish() {
             Ok((_, res)) => Ok(res),
