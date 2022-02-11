cc86eef1d9f4b9d303f15cf6efa351e3c9cd2371
Zura Benashvili <zura.benashvili@gmail.com>
2022-02-12 02:47:21 +0400

    refactor(shell): remove bootstrap actors

--- shell-integration/src/messages.rs
+++ shell-integration/src/messages.rs
@@ -1,12 +1,7 @@
 // Copyright (c) SimpleStaking, Viable Systems and Tezedge Contributors
 // SPDX-License-Identifier: MIT
 
-use std::sync::Arc;
-
 use serde::Serialize;
-
-use crypto::hash::ChainId;
-use storage::BlockHeaderWithHash;
 
 #[derive(Serialize, Debug)]
 pub struct WorkerStatus {
@@ -29,33 +24,4 @@ pub struct Prevalidator {
     // TODO: missing Tezos fields
     // information
     // pipelines
-}
-
-/// Module which holds all dedicated struct/enums for notifications used by notifiers
-pub mod notifications {
-
-    use super::*;
-
-    #[derive(Debug)]
-    pub struct NewCurrentHeadNotification {
-        pub chain_id: Arc<ChainId>,
-        pub block: Arc<BlockHeaderWithHash>,
-        pub is_bootstrapped: bool,
-    }
-
-    impl NewCurrentHeadNotification {
-        pub fn new(
-            chain_id: Arc<ChainId>,
-            block: Arc<BlockHeaderWithHash>,
-            is_bootstrapped: bool,
-        ) -> Self {
-            Self {
-                chain_id,
-                block,
-                is_bootstrapped,
-            }
-        }
-    }
-
-    pub type NewCurrentHeadNotificationRef = Arc<NewCurrentHeadNotification>;
 }
